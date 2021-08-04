d4d053c8d2f8883cb14d610eb41e40c7c64ef4a4
bildzeitung <bildzeitung@gmail.com>
2021-08-04 01:19:06 -0400

    Add project files.

---
+++ CP380-B3-BlockBlazor/Data/BlockService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace CP380_B3_BlockBlazor.Data
+{
+    public class BlockService
+    {
+        // TODO: Add variables for the dependency-injected resources
+        //       - httpClient
+        //       - configuration
+        //
+
+        //
+        // TODO: Add a constructor with IConfiguration and IHttpClientFactory arguments
+        //
+
+        //
+        // TODO: Add an async method that returns an IEnumerable<Block> (list of Blocks)
+        //       from the web service
+        //
+
+    }
+}
+}
---
+++ CP380-B3-BlockBlazor/Data/PendingTransactionService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+
+
+namespace CP380_B3_BlockBlazor.Data
+{
+    public class PendingTransactionService
+    {
+        // TODO: Add variables for the dependency-injected resources
+        //       - httpClient
+        //       - configuration
+        //
+
+        //
+        // TODO: Add a constructor with IConfiguration and IHttpClientFactory arguments
+        //
+
+        //
+        // TODO: Add an async method that returns an IEnumerable<Payload> (list of Payloads)
+        //       from the web service
+        //
+
+        //
+        // TODO: Add an async method that returns an HttpResponseMessage
+        //       and accepts a Payload object.
+        //       This method should POST the Payload to the web API server
+        //
+    }
+}
